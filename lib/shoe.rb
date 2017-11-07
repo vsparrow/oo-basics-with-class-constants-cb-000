b34970582930125e4eaba3dac3d5a948a11cc1d6
Mohammad Khan <sparrowangels@gmail.com>
2017-11-07 04:43:51 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,25 +1,34 @@
 require 'pry'
 
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
   BRANDS = []
-
   def initialize(brand)
-    @brand = brand
-    # BRANDS << brand #
-    # !(BRANDS.include?(brand)) { BRANDS << brand}
-    puts "# *******BRANDS.include?(brand): #{BRANDS.include?(brand)} || brand : #{brand} || BRANDS : #{BRANDS}"
+    @brand=brand
     if !BRANDS.include?(brand)
       BRANDS << brand
-    end
-    puts "BRANDS ARRAY AFTER: #{BRANDS}"
-
-  end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-
+    end #if
+  end #initialize
 end
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+#   BRANDS = []
+#
+#   def initialize(brand)
+#     @brand = brand
+#     # BRANDS << brand #
+#     # !(BRANDS.include?(brand)) { BRANDS << brand}
+#     puts "# *******BRANDS.include?(brand): #{BRANDS.include?(brand)} || brand : #{brand} || BRANDS : #{BRANDS}"
+#     if !BRANDS.include?(brand)
+#       BRANDS << brand
+#     end
+#     puts "BRANDS ARRAY AFTER: #{BRANDS}"
+#
+#   end
+#
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+#
+# end
